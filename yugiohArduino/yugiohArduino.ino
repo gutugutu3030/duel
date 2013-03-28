d7508278eab806da9d6f1db13a90567f38b557cc
akiyama yoh <gutugutu3030@gmail.com>
2013-03-28 14:20:21 +0900

    攻守判定追加
    モンスターの攻守判定を追加したことにより、使用ピン数が5+5=10から5*2+5=15に増えた。なお、攻撃表示のとき１、守備表示のとき２が送信されるものとする。

--- yugiohArduino/yugiohArduino.ino
+++ yugiohArduino/yugiohArduino.ino
@@ -1,4 +1,4 @@
-#define PIN 10
+#define PIN 15
 int pin[PIN];
 
 void setup() {
@@ -15,10 +15,15 @@ void setup() {
 
 void loop() {
   if (Serial.available()>0) {
-    for (int i=0;i<PIN-1;i++) {
+    for (int i=0;i<10;i+=2) {
       if (digitalRead(pin[i])==HIGH)Serial.print("1");
+      else if(digitalRead(pin[i+1])==HIGH)Serial.print("2");
       else Serial.print("0");
       Serial.print(",");
+    }
+    for(int i=10;i<PIN-1){
+      if (digitalRead(pin[i])==HIGH)Serial.print("1");
+      else Serial.print("0");
     }
     if (digitalRead(pin[PIN-1])==HIGH)Serial.println("1");
     else Serial.println("0");
