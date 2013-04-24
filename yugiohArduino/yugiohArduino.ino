3e23d11d7ecec1fade567d22cb7e6881a0e77f6c
gutugutu3030 <gutugutu3030@gmail.com>
2013-04-24 09:19:12 +0900

    攻守判定実装（仮）
    Arduinoにて攻守判定をつけた。
    今は両方のピンが反応した場合を攻撃表示としている（要確認）
    ただ、どちらのピンが反応した場合が守備表示（仮）なのかわからないので確認すること。
    
    なお通信プロトコルを整理する必要あり。

--- yugiohArduino/yugiohArduino.ino
+++ yugiohArduino/yugiohArduino.ino
@@ -1,9 +1,14 @@
-#define PIN 15
+/*
+今回はモンスターゾーンのスイッチだけ監視
+*/
+
+#define PIN 10
+
 int pin[PIN];
 
 void setup() {
   for (int i=0;i<PIN;i++) {
-    pin[i]=i+2;
+    pin[i]=i+3;
   }
 
   for (int i=0;i<PIN;i++) {
@@ -15,19 +20,17 @@ void setup() {
 
 void loop() {
   if (Serial.available()>0) {
-    for (int i=0;i<10;i+=2) {
-      if (digitalRead(pin[i])==HIGH)Serial.print("1");
-      else if(digitalRead(pin[i+1])==HIGH)Serial.print("2");
-      else Serial.print("0");
-      Serial.print(",");
-    }
-    for(int i=10;i<PIN-1){
-      if (digitalRead(pin[i])==HIGH)Serial.print("1");
-      else Serial.print("0");
-    }
-    if (digitalRead(pin[PIN-1])==HIGH)Serial.println("1");
-    else Serial.println("0");
     Serial.read();
+    delay(500);//通信の頻度を下げないと攻撃守備判定をミスる
+    for (int i=0;i<10;i+=2) {
+      if(digitalRead(pin[i])&&digitalRead(pin[i+1]))Serial.print("2,");
+      else if(digitalRead(pin[i]))Serial.print("1,");
+      else Serial.print("0,");
+    }
+    for(int i=10;i<20){
+      Serial.print("0,");
+    }
+    Serial.println();
   }
 }
 
